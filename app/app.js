4481356534d7f43f273eae47c41f048222e545f5
troyth <troyth@gmail.com>
2014-07-14 17:49:15 -0400

    body-parser update

--- app/app.js
+++ app/app.js
@@ -15,10 +15,18 @@ var morgan         = require('morgan');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
-app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); 					// log every request to the console
-app.use(bodyParser()); 						// pull information from html in POST
-app.use(methodOverride()); 					// simulate DELETE and PUT
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+// log every request to the console
+app.use(morgan('dev'));
+
+// pull information from html in POST
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+// simulate DELETE and PUT
+app.use(methodOverride());
 
 // ROUTES
 // ==============================================

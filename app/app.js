d24e356447033d76bc41db52ff9ffd7d7ef96c01
troyth <troyth@gmail.com>
2014-07-14 18:00:50 -0400

    test

--- app/app.js
+++ app/app.js
@@ -5,26 +5,38 @@
 
 var express = require('express');
 var app     = express();
-var port    = 	process.env.PORT || 1337;
+var port    = process.env.PORT || 1337;
+
+// MIDDLEWARE
+// ==============================================
+
+var logger         = require('morgan');
+var bodyParser     = require('body-parser');
+var methodOverride = require('method-override');
+
+// set the static files location /public/img will be /img for users
+app.use(express.static(__dirname + '/public'));
+
+// log every request to the console
+app.use(logger('dev'));
+
+// pull information from html in POST
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+// simulate DELETE and PUT
+app.use(methodOverride());
 
 // ROUTES
 // ==============================================
-
-// sample route with a route the way we're used to seeing it
-app.get('/sample', function(req, res) {
-	res.send('this is a sample!');
-});
-
-// we'll create our routes here
 
 // get an instance of router
 var router = express.Router();
 
 // route middleware that will happen on every request
 router.use(function(req, res, next) {
-
-	// log each request to the console
-	console.log(req.method, req.url);
 
 	// continue doing what we were doing and go to the route
 	next();
@@ -33,29 +45,6 @@ router.use(function(req, res, next) {
 // home page route (http://localhost:8080)
 router.get('/', function(req, res) {
 	res.send('im the home page!');
-});
-
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res) {
-	res.send('im the about page!');
-});
-
-// route middleware to validate :name
-router.param('name', function(req, res, next, name) {
-	// do validation on name here
-	// blah blah validation
-	// log something so we know its working
-	console.log('doing name validations on ' + name);
-
-	// once validation is done save the new item in the req
-	req.name = name;
-	// go to the next thing
-	next();
-});
-
-// route with parameters (http://localhost:8080/hello/:name)
-router.get('/hello/:name', function(req, res) {
-	res.send('hello ' + req.name + '!');
 });
 
 // apply the routes to our application

ac6e9582c1dd9d9c0c71803eb39a6898529573d8
troyth <troyth@gmail.com>
2014-07-14 17:53:01 -0400

    test

--- app/app.js
+++ app/app.js
@@ -5,55 +5,17 @@
 
 var express = require('express');
 var app     = express();
-var port    = process.env.PORT || 1337;
-
-
-// MIDDLEWARE
-// ==============================================
-
-/*
-var morgan         = require('morgan');
-var bodyParser     = require('body-parser');
-var methodOverride = require('method-override');
-
-// set the static files location /public/img will be /img for users
-app.use(express.static(__dirname + '/public'));
-
-// log every request to the console
-app.use(morgan('dev'));
-
-// pull information from html in POST
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(bodyParser.json());
-
-// simulate DELETE and PUT
-app.use(methodOverride());
-*/
+var port    = 	process.env.PORT || 1337;
 
 // ROUTES
 // ==============================================
 
-// get an instance of router
-var router = express.Router();
-
-console.log('express:');
-console.dir(express);
-
-// home page route (http://localhost:8080)
-router.get('/', function(req, res) {
-	res.send('im the home page!');
+// sample route with a route the way we're used to seeing it
+app.get('/sample', function(req, res) {
+	res.send('this is a sample!');
 });
 
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res) {
-	res.send('im the about page!');
-});
-
-// apply the routes to our application
-app.use('/', router);
-
+// we'll create our routes here
 
 // START THE SERVER
 // ==============================================

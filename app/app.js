ca2e6f9eb968b8dea36e40b589f099735d0a7884
Troy Conrad Therrien <troyth@gmail.com>
2014-07-14 17:36:19 -0400

    express with ejs

--- app/app.js
+++ app/app.js
@@ -9,17 +9,17 @@ var express = require('express')
   , http = require('http')
   , path = require('path');
 
-var bodyParser = require('body-parser'),
-    session = require('serve-session'),
-        
-
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 1337);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
-app.use(bodyParser());
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
 // development only

060c81e29bc2efb8c897c8a9109a54eb1d9fde50
troyth <troyth@gmail.com>
2014-07-14 18:33:43 -0400

    add serialize for passport

--- app/app.js
+++ app/app.js
@@ -53,6 +53,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Tumblr profile is serialized
+//   and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
 // Use the TumblrStrategy within Passport.
 //   Strategies in passport require a `verify` function, which accept
 //   credentials (in this case, a token, tokenSecret, and Tumblr profile), and

f08daa6bb3c834836acde4222d163fc10c945777
Troy Conrad Therrien <troyth@gmail.com>
2014-07-14 17:34:45 -0400

    express app generated

---
+++ app/app.js
@@ -0,0 +1,35 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , routes = require('./routes')
+  , user = require('./routes/user')
+  , http = require('http')
+  , path = require('path');
+
+var bodyParser = require('body-parser'),
+    session = require('serve-session'),
+        
+
+var app = express();
+
+// all environments
+app.set('port', process.env.PORT || 1337);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/users', user.list);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
